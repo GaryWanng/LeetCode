cd9f3866f093ce868f150a5ec4f91ddfafe2c07b
GaryWanng <1357954199@qq.com>
2025-08-10 14:58:29 +0800

    rotate array

--- src/main/java/com/example/bootproject/leetcode/RotateArray.java
+++ src/main/java/com/example/bootproject/leetcode/RotateArray.java
@@ -1,7 +1,82 @@
 package com.example.bootproject.leetcode;
 
+import java.util.Arrays;
+
 /**
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
  *
+ * 示例 1:
+ *
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右轮转 1 步: [7,1,2,3,4,5,6]
+ * 向右轮转 2 步: [6,7,1,2,3,4,5]
+ * 向右轮转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ *
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右轮转 1 步: [99,-1,-100,3]
+ * 向右轮转 2 步: [3,99,-1,-100]
  */
 public class RotateArray {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        int[] nums2 = new int[]{-1,-100,3,99};
+        rotate(nums, 3);
+        rotate(nums2, 2);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(nums2));
+    }
+
+    public static void rotate(int[] nums, int k) {
+        // 实际移动次数
+        int moveCount = k % nums.length;
+        int[] head = new int[moveCount];
+        //如果相等等于不需要移动，返回原数组
+        if (moveCount > 0) {
+            for (int i = nums.length - 1; i >= 0; i--) {
+                //倒着把值放进head数组里
+                if (nums.length - i <= moveCount) {
+                    head[moveCount - (nums.length - i)] = nums[i];
+                }
+
+                if (i > moveCount - 1) {
+                    nums[i] = nums[i - moveCount];
+                } else {
+                    nums[i] = head[i];
+                }
+            }
+        }
+    }
+
+    /**
+     * 数组反转，先翻转整个数组，
+     * 1. 我们可以先将所有元素翻转，这样尾部的 k mod n 个元素就被移至数组头部
+     * 2. 然后我们再翻转 [0,k mod n−1] 区间的元素
+     * 3. 翻转[k mod n,n−1] 区间的元素即能得到最后的答案
+     *
+     * @param nums
+     * @param k
+     */
+    public static void rotate2(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+
 }
